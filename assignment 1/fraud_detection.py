bc431ad14923fe4c228917dd7b0859e1414f56f7
xhy <ncb21992@hotmail.com>
2017-05-12 13:56:33 +0200

    fraud_detection now works

--- assignment 1/fraud_detection.py
+++ assignment 1/fraud_detection.py
@@ -21,7 +21,7 @@
 
 # In[7]:
 
-get_ipython().magic(u'matplotlib inline')
+get_ipython().magic(u'matplotlib qt5')
 import datetime
 import time
 import matplotlib.pyplot as plt
@@ -41,25 +41,28 @@ def string_to_timestamp(date_string):  # convert time string to float value
 
 
 def aggregate(before_aggregate, aggregate_feature):
+    print('aggregate feature: '+ str(aggregate_feature))
+    after_aggregate = []
     if aggregate_feature == 'day':
-        after_aggregate = []
         pos_date = -1
-        before_aggregate.sort(key = itemgetter(9))  # sort by timestamp
+        before_aggregate.sort(key=itemgetter(9))  # sort by timestamp
         temp = groupby(before_aggregate, itemgetter(-2))
         group_unit = []
         mean = []
         for i, item in temp:  # i is group id
             for jtem in item:  # unit in each group
                 group_unit.append(jtem)
-            # for feature_i in xrange(6):
-            #     mean.append(zip(group_unit)[feature_i])
+            #print('group_unit: ' + str(group_unit))
             # after_aggregate.append(group_unit)
+            # for feature_i in range(6):
+            #     mean.append(list(zip(group_unit))[feature_i])
+
+            # print('mean: ' + str(mean))
             after_aggregate.append(mean)
             group_unit = []
         # print after_aggregate[0]
         # print before_aggregate[0]
     if aggregate_feature == 'client':
-        after_aggregate = []
         pos_client = -3
         before_aggregate.sort(key = itemgetter(pos_client))  # sort with cardID firstly，if sort with 2 feature, itemgetter(num1,num2)
         temp = groupby(before_aggregate, itemgetter(pos_client))  # group
@@ -73,7 +76,7 @@ def aggregate(before_aggregate, aggregate_feature):
 
 
 def aggregate_mean(before_aggregate):
-    print before_aggregate[0]
+    print(before_aggregate[0])
     if True:
         after_aggregate = []
         pos_date = -1
@@ -99,6 +102,8 @@ def aggregate_mean(before_aggregate):
         # print after_aggregate[0]
         # print before_aggregate[0]
     return after_aggregate
+
+
 if __name__ == "__main__":
     src = 'data/data_for_student_case.csv'
     ah = open(src, 'r')
@@ -107,14 +112,15 @@ if __name__ == "__main__":
     data = []
     color = []
     (issuercountry_set, txvariantcode_set, currencycode_set, shoppercountry_set, interaction_set,
-    verification_set, accountcode_set, mail_id_set, ip_id_set, card_id_set) = [set() for _ in xrange(10)]
+    verification_set, accountcode_set, mail_id_set, ip_id_set, card_id_set) = [set() for _ in range(10)]
     (issuercountry_dict, txvariantcode_dict, currencycode_dict, shoppercountry_dict, interaction_dict,
-    verification_dict, accountcode_dict, mail_id_dict, ip_id_dict, card_id_dict) = [{} for _ in xrange(10)]
+    verification_dict, accountcode_dict, mail_id_dict, ip_id_dict, card_id_dict) = [{} for _ in range(10)]
     # label_set
     # cvcresponse_set = set()
     ah.readline()# skip first line
     for line_ah in ah:
-        if line_ah.strip().split(',')[9]=='Refused':  # remove the row with 'refused' label, since it's uncertain about fraud
+        # remove the row with 'refused' label, since it's uncertain about fraud
+        if line_ah.strip().split(',')[9]=='Refused':
             continue
         if 'na' in str(line_ah.strip().split(',')[14]).lower() or 'na' in str(line_ah.strip().split(',')[4].lower()):
             continue
@@ -138,20 +144,20 @@ if __name__ == "__main__":
         verification = line_ah.strip().split(',')[10]  # shopper provide CVC code or not
         verification_set.add(verification)
         cvcresponse = line_ah.strip().split(',')[11]  # 0 = Unknown, 1=Match, 2=No Match, 3-6=Not checked
-        if cvcresponse > 2:
+        if int(cvcresponse) > 2:
             cvcresponse = 3
-        year_info = datetime.datetime.strptime(line_ah.strip().split(',')[12],'%Y-%m-%d %H:%M:%S').year
-        month_info = datetime.datetime.strptime(line_ah.strip().split(',')[12],'%Y-%m-%d %H:%M:%S').month
-        day_info = datetime.datetime.strptime(line_ah.strip().split(',')[12],'%Y-%m-%d %H:%M:%S').day
+        year_info = datetime.datetime.strptime(line_ah.strip().split(',')[12], '%Y-%m-%d %H:%M:%S').year
+        month_info = datetime.datetime.strptime(line_ah.strip().split(',')[12], '%Y-%m-%d %H:%M:%S').month
+        day_info = datetime.datetime.strptime(line_ah.strip().split(',')[12], '%Y-%m-%d %H:%M:%S').day
         creationdate = str(year_info)+'-'+str(month_info)+'-'+str(day_info)  # Date of transaction
         creationdate_stamp = string_to_timestamp(line_ah.strip().split(',')[12])  # Date of transaction-time stamp
         accountcode = line_ah.strip().split(',')[13]  # merchant’s webshop
         accountcode_set.add(accountcode)
-        mail_id = int(float(line_ah.strip().split(',')[14].replace('email','')))  # mail
+        mail_id = int(float(line_ah.strip().split(',')[14].replace('email', '')))  # mail
         mail_id_set.add(mail_id)
-        ip_id = int(float(line_ah.strip().split(',')[15].replace('ip','')))  # ip
+        ip_id = int(float(line_ah.strip().split(',')[15].replace('ip', '')))  # ip
         ip_id_set.add(ip_id)
-        card_id = int(float(line_ah.strip().split(',')[16].replace('card','')))  # card
+        card_id = int(float(line_ah.strip().split(',')[16].replace('card', '')))  # card
         card_id_set.add(card_id)
         data.append([issuercountry, txvariantcode, issuer_id, amount, currencycode,
                     shoppercountry, interaction, verification, cvcresponse, creationdate_stamp,
@@ -166,7 +172,7 @@ if __name__ == "__main__":
     plt.figure(1)
     plt.plot(transaction_num_day, color = 'c', linewidth = 2)
     plt.plot()
-    plt.text(2,0.0,'Date: 2015-10-8')
+    # plt.text(2,0.0,'Date: 2015-10-8')
     plt.xlabel('Date')
     plt.ylabel('Number of Transactions')
     plt.xlim([0,125])
@@ -182,6 +188,17 @@ if __name__ == "__main__":
     plt.ylabel('Number of Transactions')
     plt.axis('tight')
     plt.savefig('Client Aggregating.png')
+
+
+
+# todo visualization task
+
+# todo imbalance task
+
+# todo black box classification task
+
+# todo white box classification task
+
 
 
 # In[6]:
@@ -204,7 +221,7 @@ for item in list(verification_set):
     verification_dict[item] = list(verification_set).index(item)
 for item in list(accountcode_set):
     accountcode_dict[item] = list(accountcode_set).index(item)
-print len(list(card_id_set))
+print(len(list(card_id_set)))
 # for item in list(card_id_set):
 #     card_id_dict[item] = list(card_id_set).index(item)
 '''modify categorial feature to number in data set'''
@@ -241,25 +258,28 @@ x_array = np.array(x)
 y_array = np.array(y)
 usx = x_array
 usy = y_array
-x_train, x_test, y_train, y_test = train_test_split(usx, usy, test_size = 0.2)#test_size: proportion of train/test data
+x_train, x_test, y_train, y_test = train_test_split(usx, usy, test_size = 0.2)  # test_size: proportion of train/test data
 clf = neighbors.KNeighborsClassifier(algorithm = 'kd_tree')
 clf.fit(x_train, y_train)
 y_predict = clf.predict(x_test)
-for i in xrange(len(y_predict)):
-    if y_test[i]==1 and y_predict[i]==1:
+for i in range(len(y_predict)):
+    if y_test[i] == 1 and y_predict[i] == 1:
         TP += 1
-    if y_test[i]==0 and y_predict[i]==1:
+    if y_test[i] == 0 and y_predict[i] == 1:
         FP += 1
-    if y_test[i]==1 and y_predict[i]==0:
+    if y_test[i] == 1 and y_predict[i] == 0:
         FN += 1
-    if y_test[i]==0 and y_predict[i]==0:
+    if y_test[i] == 0 and y_predict[i] == 0:
         TN += 1
-print 'TP: '+ str(TP)
-print 'FP: '+ str(FP)
-print 'FN: '+ str(FN)
-print 'TN: '+ str(TN)
-# print confusion_matrix(y_test, answear) watch out the element in confusion matrix
+print('TP, True Positive: '+ str(TP))
+print('FP, False Positive: '+ str(FP))
+print('FN, False Negative: '+ str(FN))
+print('TN, True Negative: '+ str(TN))
+# print confusion_matrix(y_test, answer) watch out the element in confusion matrix
 precision, recall, thresholds = precision_recall_curve(y_test, y_predict)
 predict_proba = clf.predict_proba(x_test)  # the probability of each sample labelled to positive or negative
 
+print('precision: ' + str(precision))
+print('recall: ' + str(recall))
+print('thresholds: ' + str(thresholds))
 
---
+++ assignment 1/run.sh
@@ -0,0 +1,2 @@
+#do not forget to run sh initialize.sh to have prerequisites installed.
+ipython3 fraud_detection.py
